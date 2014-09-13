ca306157c996c54519b28c58cc5ea7c2f60d27a6
Graham Bell <g.bell@jach.hawaii.edu>
2014-09-12 14:03:08 -1000

    Prepare CPAN release 1.23

--- lib/App/SourcePlot.pm
+++ lib/App/SourcePlot.pm
@@ -22,7 +22,7 @@ display a plot of astronomical sources on adjustable axes.
 use strict;
 #use warnings;
 
-our $VERSION = '1.22';
+our $VERSION = '1.23';
 
 use Config::IniFiles;
 use Tk;
--- lib/App/SourcePlot/Source.pm
+++ lib/App/SourcePlot/Source.pm
@@ -29,7 +29,7 @@ use Math::Trig qw/pi/;
 use DateTime;
 use DateTime::Format::Strptime;
 
-our $VERSION = '1.22';
+our $VERSION = '1.23';
 
 my $locateBug = 0;
 
