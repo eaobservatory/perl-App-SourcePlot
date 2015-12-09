befeeb51a129cde955fa074fac0f77707b2149c9
Graham Bell <g.bell@eaobservatory.org>
2015-12-09 11:28:05 -1000

    Prepare CPAN release 1.25

--- lib/App/SourcePlot/Plotter/Tk.pm
+++ lib/App/SourcePlot/Plotter/Tk.pm
@@ -21,7 +21,7 @@ use vars qw/$VERSION/;
 # Load Tk module
 use Tk;
 
-$VERSION = '1.24';
+$VERSION = '1.25';
 
 =head1 EXTERNAL MODULES
 
--- lib/App/SourcePlot/Source.pm
+++ lib/App/SourcePlot/Source.pm
@@ -29,7 +29,7 @@ use Math::Trig qw/pi/;
 use DateTime;
 use DateTime::Format::Strptime;
 
-our $VERSION = '1.24';
+our $VERSION = '1.25';
 
 my $locateBug = 0;
 
